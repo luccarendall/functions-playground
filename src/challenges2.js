9850bb5af5737121fa92a45401a8a2b71cee6c58
Lucca Rendall <luccarendall1@gmail.com>
2021-12-06 14:42:16 -0300

    Requisitos 10 e 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,15 @@
 // Desafio 10
-function techList() {
+  function techList(techs, name) {
+  let list = [];
+  let sortedTechs = techs.sort();
+  for (let tech of sortedTechs) {
+    list.push({
+      tech,
+      name,
+    });
+  }
+  if (techs.length === 0) return 'Vazio!';
+  return list;
 }
 
 // Desafio 11
@@ -13,8 +23,11 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(texto) {
+  let result = texto.match(/[0-9]/g);
+  let aguaCopos = 0;
+  for (let item of result) aguaCopos += parseInt(item, 10);
+  return (aguaCopos === 1) ? '1 copo de água' : `${aguaCopos} copos de água`;
 }
 
 module.exports = {
